ccbc7bd3872f073d1752e303e856bb99726d202e
Michael Zetterholm <michael.zetterholm@gmail.com>
2021-10-23 16:17:04 +0200

    Remove getStuff query

--- src/api/mocks/server.ts
+++ src/api/mocks/server.ts
@@ -1,5 +1,4 @@
 import { GET_ACTIVITY } from '../queries/getActivity';
-import { GET_STUFF } from '../queries/getStuff';
 
 export const mocks = [
   {
@@ -12,16 +11,6 @@ export const mocks = [
           { id: '1', name: 'Yoga', date: new Date('2021-10-22').toISOString() },
           { id: '2', name: 'Running', date: new Date('2021-10-23').toISOString() },
         ],
-      },
-    },
-  },
-  {
-    request: {
-      query: GET_STUFF,
-    },
-    result: {
-      data: {
-        stuff: [{ id: '1', name: 'Buck' }],
       },
     },
   },
