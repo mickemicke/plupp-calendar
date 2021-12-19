df5a3233c77f7f3536eac4acc1805bdeac69290c
Michael Zetterholm <michael.zetterholm@antura.se>
2021-12-19 12:15:49 +0100

    Add pnpm and relevant folder structure

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,8 @@
 module.exports = {
   parser: '@typescript-eslint/parser', // Specifies the ESLint parser
   extends: [
+    "react-app",
+    "react-app/jest",
     "plugin:@typescript-eslint/recommended",
     "prettier"
   ],
